9ef5f5c7570889eecf924f8d42367b0e2da648a7
EbbeVang <ev@evang.dk>
2019-02-22 09:35:47 +0100

    Initial commit

---
+++ DelegateBankAccountExample/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBankAccountExample
+{
+    class Account
+    {
+        private int _saldo;
+
+        public delegate int Deposit(int deposit);
+
+        public delegate int Withdrawal(int withdrawal);
+
+        public Deposit AccountDeposit;
+
+        public Withdrawal AccountWithdrawal;
+
+        // varianter til brug i mine delegates:
+        public int DepositNoAlerts(int deposit)
+        {
+            return 0;
+        }
+
+
+        public int Saldo
+        {
+            get { return _saldo; }
+        }
+    }
+}
---
+++ DelegateBankAccountExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateBankAccountExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account NikolajsAccount = new Account();
+            NikolajsAccount.AccountDeposit = NikolajsAccount.DepositNoAlerts;
+
+            Console.WriteLine(NikolajsAccount.AccountDeposit(3));
+
+        }
+    }
+}
